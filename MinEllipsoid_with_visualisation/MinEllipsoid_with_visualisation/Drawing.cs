7058f466900c511436da56dad4958cd35e5bf5b2
MSolodarenko <solomaks1995@gmail.com>
2015-02-14 17:03:26 +0200

    Refactoring
    Minor code refactoring in Drawing section

--- MinEllipsoid_with_visualisation/MinEllipsoid_with_visualisation/Drawing.cs
+++ MinEllipsoid_with_visualisation/MinEllipsoid_with_visualisation/Drawing.cs
@@ -48,45 +48,47 @@ namespace MinEllipsoid_with_visualisation
                     GL.LoadIdentity();
                     GL.Ortho(-1.0, 1.0, -1.0, 1.0, 0.0, 4.0);
 
-                    GL.Begin(PrimitiveType.Lines);
-                    GL.Color3(Color.Green);
-                    GL.Vertex2(-1, 0);
-                    GL.Vertex2(1, 0);
-                    GL.Vertex2(0, 1);
-                    GL.Vertex2(0, -1);
-                    GL.End();
-
-                    //GL.Begin(PrimitiveType.Points);
-
-                    for (int i = 0; i < p.num_of_points; ++i)
-                    {
-                        //GL.Color3(Color.Red);
-                        //GL.Vertex3(p.points[i]);
-                        
-                        GL.Color3(1.0, 1.0, 0.6);
-
-                        GL.Begin(PrimitiveType.TriangleFan);
-                        GL.Vertex3(p.points[i]);
-                        double radius = 0.01;
-                        for (double angle = 1.0f; angle < 361.0f; angle += 0.2)
-                        {
-                            double x2 = p.points[i].X + Math.Sin(angle) * radius;
-                            double y2 = p.points[i].Y + Math.Cos(angle) * radius;
-                            GL.Vertex2(x2, y2);
-                        }
-
-                        GL.End();
-                    }
-                    
-                    //GL.End();
+                    Decarts_Lines();
+                    DrawPoints(p);
 
                     game.SwapBuffers();
                 };
 
                 // Run the game at 60 updates per second
                 game.Run(60.0);
+            }    
+        }
+        public static void DrawPoints(Points p)     
+        {
+            for (int i = 0; i < p.num_of_points; ++i)
+            {
+                GL.Color3(Color.Red);
+                Circle(p.points[i]);
             }
-             
+        }
+        public static void Circle(Vector3d a)      
+        {
+            GL.Begin(PrimitiveType.TriangleFan);
+            GL.Vertex3(a);
+            double radius = 0.005;
+            for (double angle = 1.0f; angle < 361.0f; angle += 0.2)
+            {
+                double x2 = a.X + Math.Sin(angle) * radius;
+                double y2 = a.Y + Math.Cos(angle) * radius;
+                double z2 = a.Z;
+                GL.Vertex3(x2, y2, z2);
+            }
+            GL.End();
+        }
+        public static void Decarts_Lines()
+        {
+            GL.Begin(PrimitiveType.Lines);
+            GL.Color3(Color.Green);
+            GL.Vertex2(-1, 0);
+            GL.Vertex2(1, 0);
+            GL.Vertex2(0, 1);
+            GL.Vertex2(0, -1);
+            GL.End();
         }
     }
 }
