9a0bc003c6d7ad1fa3da5135eb551266ee7acbf0
MSolodarenko <solomaks1995@gmail.com>
2015-05-17 13:49:49 +0300

    Class Ellipsoid
    which contains 3 parameters: a,b,c and field Volume

---
+++ MinEllipsoid/MinEllipsoid/Ellipsoid.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinEllipsoid
+{
+    class Ellipsoid
+    {
+        public double a, b, c;
+        public double V;
+        public Ellipsoid(double x, double y, double z) { a = x; b = y; c = z; }
+        public Ellipsoid() { }
+    }
+}
